172f4cd38c3ebf4e0a5665a9401b98206d36f3e9
JiheiBot <2757174746@qq.com>
2023-12-31 18:07:03 +0000

    update
    which could also affect performance of the server and potentially decrease UX

--- StockMarketDataAnalysisWebAPI_SAAS/StockMarketDataAnalysisWebAPI_SAAS/Controllers/StockMarketDataAnalysisController.cs
+++ StockMarketDataAnalysisWebAPI_SAAS/StockMarketDataAnalysisWebAPI_SAAS/Controllers/StockMarketDataAnalysisController.cs
@@ -7,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace StockMarketDataAnalysisWebAPI_SAAS.Controllers
 {
-    //may have thounds of request, each request requires a thread from thread pool
+    //may have thounds of request, each request requires a thread from thread pool.
+    //and it requires unnessary threads to run the request which slow down the performance.
+    //affect performance of the server and potentially decerase UX
     [Route("api/[controller]")]
     [ApiController]
     public class StockMarketDataAnalysisController : ControllerBase
