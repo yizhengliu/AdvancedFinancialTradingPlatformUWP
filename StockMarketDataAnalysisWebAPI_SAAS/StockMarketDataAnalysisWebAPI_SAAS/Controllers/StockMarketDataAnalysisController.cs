108e1540ec66c2cb0e94b11bbccda178eb578b89
JiheiBot <2757174746@qq.com>
2023-12-31 18:10:18 +0000

    Update StockMarketDataAnalysisController.cs
    always only put async on the client side at the closest part for example the event handler. IO does not require additional thread, but CPU needs which could potentially decrease UX and performance

--- StockMarketDataAnalysisWebAPI_SAAS/StockMarketDataAnalysisWebAPI_SAAS/Controllers/StockMarketDataAnalysisController.cs
+++ StockMarketDataAnalysisWebAPI_SAAS/StockMarketDataAnalysisWebAPI_SAAS/Controllers/StockMarketDataAnalysisController.cs
@@ -10,6 +10,7 @@ namespace StockMarketDataAnalysisWebAPI_SAAS.Controllers
     //may have thounds of request, each request requires a thread from thread pool.
     //and it requires unnessary threads to run the request which slow down the performance.
     //affect performance of the server and potentially decerase UX
+    //avoid running background threads on the server side rather run oprations synchrononously on the server side
     [Route("api/[controller]")]
     [ApiController]
     public class StockMarketDataAnalysisController : ControllerBase
