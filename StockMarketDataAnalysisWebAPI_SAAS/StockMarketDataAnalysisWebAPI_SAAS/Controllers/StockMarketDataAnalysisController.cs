33066fe4a31f9a8900cf1e3b8218c8a7e934ede1
JiheiBot <2757174746@qq.com>
2023-12-31 18:06:07 +0000

    Create StockMarketDataAnalysisController.cs
    Last best practice update: it is good to not run async in the server side code as the client already has requested other threads to wait for server respond, server has already grab a service thread and it is bad to grab more threads for one operation that will cost more server performance

---
+++ StockMarketDataAnalysisWebAPI_SAAS/StockMarketDataAnalysisWebAPI_SAAS/Controllers/StockMarketDataAnalysisController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockMarketDataAnalysisWebAPI_SAAS.Controllers
+{
+    //may have thounds of request, each request requires a thread from thread pool
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockMarketDataAnalysisController : ControllerBase
+    {
+    }
+}
